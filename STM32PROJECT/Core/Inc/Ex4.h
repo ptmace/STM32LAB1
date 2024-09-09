732f8ca9b7b7fd08a64712fd626d636de8eae2bc
ptmace <anh.phambk107@hcmut.edu.vn>
2024-09-09 11:13:26 +0700

    Init for Ex4

---
+++ STM32PROJECT/Core/Inc/Ex4.h
@@ -0,0 +1,15 @@
+/*
+ * Ex4.h
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: 2004p
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+
+#include "main.h"
+
+void display7SEG(int number);
+
+#endif /* INC_EX4_H_ */
