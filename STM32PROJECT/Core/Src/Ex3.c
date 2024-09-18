a5bb8009de9757669f5c74d9f91f24a98b3e6fa4
ptmace <anh.phambk107@hcmut.edu.vn>
2024-09-19 01:13:46 +0700

    Update for Ex3

--- STM32PROJECT/Core/Src/Ex3.c
+++ STM32PROJECT/Core/Src/Ex3.c
@@ -31,40 +31,40 @@ void exercise3_run(){
 		case 0:
 			init_exercise3();
 			HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
-		    HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+			HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
 
 			HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
-		    HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
-		    count10s++;
-		    break;
+			HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+			count10s++;
+			break;
 		case 2:
-		  	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
-		  	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+			HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+			HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
 
-		    HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
-		    HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
-		    count10s++;
-		    break;
+			HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+			HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+			count10s++;
+			break;
 		case 5:
-		  	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
-		    HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
-		    HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
-		    HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+			HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
+			HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+			HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+			HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
 
-		    HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
-		    HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+			HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+			HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
 
-		    HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
-		    HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
-		    count10s++;
-		    break;
+			HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+			HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+			count10s++;
+			break;
 		case 7:
 			HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
 			HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
-		  	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
-		    HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
-		    count10s++;
-		    break;
+			HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+			HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+			count10s++;
+			break;
 		case 9:
 			count10s = 0;
 			break;
