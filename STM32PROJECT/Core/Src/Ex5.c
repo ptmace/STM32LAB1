d2777cf7a62e60190bdcd62b55841787318d48ef
ptmace <anh.phambk107@hcmut.edu.vn>
2024-09-19 01:03:23 +0700

    Update for Ex5

--- STM32PROJECT/Core/Src/Ex5.c
+++ STM32PROJECT/Core/Src/Ex5.c
@@ -160,11 +160,11 @@ void exercise5_run(){
 			count++;
 			break;
 		case 2:
-		  	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
-		  	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+			HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+			HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
 
-		    HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
-		    HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+			HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+			HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
 		    display7SEG(7);
 		    count++;
 		    break;
@@ -177,7 +177,7 @@ void exercise5_run(){
 			count++;
 			break;
 		case 5:
-		  	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
+			HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
 		    HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
 		    HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
 		    HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
@@ -198,8 +198,8 @@ void exercise5_run(){
 			HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
 			HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
 		  	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
-		    HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
-		    display7SEG(2);
+		  	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+		  	display7SEG(2);
 		    count++;
 		    break;
 		case 8:
